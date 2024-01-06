c6a0d7201241e62dbf171b729cdb565c37dba0d0
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-06 15:36:44 +0200

    added mui

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -1,5 +1,15 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import T from "@mui/material/Typography"
+import Button from "@mui/material/Button"
+
 export default function Root() {
     return (
-        <p>Hello</p>
+        <ThemeProvider theme={THEME}>
+            <Button variant="contained">Make a Message Pop Up (currently unavailable)</Button>
+        </ThemeProvider>
     )
 }
+
+const THEME = createTheme({
+ });
