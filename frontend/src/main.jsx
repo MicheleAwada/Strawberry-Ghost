b8eb0459b5c0b2f7190b0f3c2f3d4d89c22bc0cf
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-06 16:17:18 +0200

    placed providers in main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,23 @@ import ReactDOM from 'react-dom/client'
 import Root from './root'
 import './index.scss'
 
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import ProvideAndRenderMessages from "./components/messages";
+
+import { MessagesContext } from "./components/messages";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <ThemeProvider theme={THEME}>
+      <ProvideAndRenderMessages>
+        <Root />
+      </ProvideAndRenderMessages>
+    </ThemeProvider>
   </React.StrictMode>,
 )
+
+const THEME = createTheme({
+ });
+
+
+ export {MessagesContext}
--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -1,20 +1,5 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import ProvideAndRenderMessages from "./components/messages";
-
-import { MessagesContext } from "./components/messages";
-
 export default function Root() {
     return (
-        <ThemeProvider theme={THEME}>
-            <ProvideAndRenderMessages>
-
-            </ProvideAndRenderMessages>
-        </ThemeProvider>
+        <></>
     )
 }
-
-const THEME = createTheme({
- });
-
-
- export {MessagesContext}
