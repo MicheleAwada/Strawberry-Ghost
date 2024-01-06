9d1648f653e93a940837aeae4140995dffe40ac8
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-06 16:20:38 +0200

    fixed theme not declared bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,9 @@ import ProvideAndRenderMessages from "./components/messages";
 
 import { MessagesContext } from "./components/messages";
 
+const THEME = createTheme({
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={THEME}>
@@ -18,9 +21,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>,
 )
-
-const THEME = createTheme({
- });
-
 
  export {MessagesContext}
