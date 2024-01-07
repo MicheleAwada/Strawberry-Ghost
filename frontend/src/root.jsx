52dbe9dfab7a26a4bba41179c72e995a85eb1666
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-07 10:20:50 +0200

    added routing/children to root element

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -1,7 +1,19 @@
-import Index from "./routes";
+import {Outlet} from "react-router"
+import { RenderMessages, MessagesContext } from "./components/messages"
+
+import Header from "./components/header"
+
+import Box from "@mui/material/Box";
 
 export default function Root() {
     return (
-        <Index />
+        <Box>
+            <RenderMessages />
+            <Header />
+            <Outlet />
+        </Box>
     )
 }
+
+
+export { MessagesContext }
