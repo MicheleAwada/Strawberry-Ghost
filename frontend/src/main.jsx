f886678a7b085404a46ef33d8fd1e4d5fdad5379
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-07 07:20:56 +0200

    color theme added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,17 @@ import Root from './root'
 import './index.scss'
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {pink, orange} from '@mui/material/colors';
+
 import ProvideAndRenderMessages from "./components/messages";
 
 import { MessagesContext } from "./components/messages";
 
 const THEME = createTheme({
+  palette : {
+    primary: {'main': pink[300]},
+    secondary: {'main': orange[500]},
+  } 
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
