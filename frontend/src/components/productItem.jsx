5331cffef8a70b01bf235b259f620309fe010c34
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-07 13:50:43 +0200

    added stretched product item

--- frontend/src/components/productItem.jsx
+++ frontend/src/components/productItem.jsx
@@ -9,59 +9,114 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 
-export function ProductCategory({ categories}) {
-    //TODO
+export function ProductCategory({ categories }) {
+	//TODO
 }
 
 export function ProductPrice({ price }) {
-    const wholeNumber = Math.floor(price)
-    const decimalNumber = price - wholeNumber
-    const wholeNumberString = wholeNumber.toString()
-    const decimalNumberString = decimalNumber.toString().padStart(2, "0")
+	const wholeNumber = Math.floor(price);
+	const decimalNumber = price - wholeNumber;
+	const wholeNumberString = wholeNumber.toString();
+	const decimalNumberString = decimalNumber.toString().padStart(2, "0");
 
-    const baseFontSize = 1.5
+	const baseFontSize = 1.5;
 
-    return (
-        <Box sx={{px:1}}>
-            <Stack flexDirection="row" sx={{position: "relative", display: "inline-flex"}}>
-                <Typography color={"initial"} fontSize={`${baseFontSize/2}rem`} sx={{position: "absolute", top: `${baseFontSize/4}rem`, left: `${1 * (baseFontSize / -3.5)}rem`}}>
-                    {"$"}
-                </Typography>
-                <Typography color={"initial"} fontSize={`${baseFontSize}rem`}>
-                    {wholeNumberString}
-                </Typography>
-                <Typography color={"initial"} fontSize={`${baseFontSize/2}rem`} sx={{position: "absolute", top: `${baseFontSize/4}rem`, right: `${2 * (baseFontSize / -3.5)}rem`}}>
-                    {decimalNumberString}
-                </Typography>
-            </Stack>
-        </Box>
-    )
+	return (
+		<Box sx={{ px: 1 }}>
+			<Stack
+				flexDirection="row"
+				sx={{ position: "relative", display: "inline-flex" }}
+			>
+				<Typography
+					color={"initial"}
+					fontSize={`${baseFontSize / 2}rem`}
+					sx={{
+						position: "absolute",
+						top: `${baseFontSize / 4}rem`,
+						left: `${1 * (baseFontSize / -3.5)}rem`,
+					}}
+				>
+					{"$"}
+				</Typography>
+				<Typography color={"initial"} fontSize={`${baseFontSize}rem`}>
+					{wholeNumberString}
+				</Typography>
+				<Typography
+					color={"initial"}
+					fontSize={`${baseFontSize / 2}rem`}
+					sx={{
+						position: "absolute",
+						top: `${baseFontSize / 4}rem`,
+						right: `${2 * (baseFontSize / -3.5)}rem`,
+					}}
+				>
+					{decimalNumberString}
+				</Typography>
+			</Stack>
+		</Box>
+	);
 }
 
 export default function ProductItem({ info }) {
 	return (
-		<Grid item xs={12} sm={6} md={4} sx={{ width: "18rem"}}>
-			<Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                <a href="#">
-                    <CardMedia
-                        component="div"
-                        sx={{
-                            pt: "56.25%",
-                            bgcolor: "inherit"
-                        }}
-                        image={info.thumbnail}
-                    />
-                </a>
-                <CardContent sx={{ flexGrow: 1 }}>
-                        <Link href="#"  variant="h5">
-                            <Typography  variant="h5" component="h2">{info.title}</Typography>
-                        </Link>
-                    <ProductPrice price={info.price} />
-                </CardContent>
+		<Grid item xs={12} sm={6} md={4} sx={{ width: "18rem" }}>
+			<Card sx={{ height: "100%" }}>
+				<a href="#">
+					<CardMedia
+						component="div"
+						sx={{
+							pt: "56.25%",
+							bgcolor: "inherit",
+						}}
+						image={info.thumbnail}
+					/>
+				</a>
+				<CardContent sx={{ flexGrow: 1 }}>
+					<Link href="#" variant="h5" sx={{ display: "inline-block" }}>
+						<Typography variant="h5" component="h2">
+							{info.title}
+						</Typography>
+					</Link>
+					<ProductPrice price={info.price} />
+				</CardContent>
 				<CardActions>
-					<Button variant="contained" size="small">Add to Cart</Button>
+					<Button variant="contained" size="small">
+						Add to Cart
+					</Button>
 				</CardActions>
 			</Card>
 		</Grid>
+	);
+}
+export function ProductItemStretch({ info }) {
+	return (
+		<Card sx={{ width: "100%", height: "12rem", display: "flex", flexDirection: "row" }}>
+                <CardMedia
+                    component="a"
+                    href="#"
+                    sx={{
+                        objectFit: "cover",
+                        aspectRatio: "4/3",
+                        maxHeight: "100%",
+                        bgcolor: "inherit",
+                        flexGrow: 1,
+                    }}
+                    image={info.thumbnail}
+                />
+
+			<Box sx={{ display: "flex", flexDirection: "column", pr: {xs: 2, sm: 6} }}>
+				<CardContent sx={{ flexGrow: 1 }}>
+					<Link href="#" variant="h6" sx={{ display: "inline-block" }}>
+						<Typography variant="h6" component="h2">
+							{info.title}
+						</Typography>
+					</Link>
+					<ProductPrice price={info.price} />
+				</CardContent>
+				<CardActions>
+					<Button variant="contained" size="small">Add to Cart</Button>
+				</CardActions>
+			</Box>
+		</Card>
 	);
 }
--- frontend/src/components/productListView.jsx
+++ frontend/src/components/productListView.jsx
@@ -1,4 +1,4 @@
-import ProductItem from "./productItem";
+import ProductItem, {ProductItemStretch} from "./productItem";
 
 import Stack from "@mui/material/Stack"
 import Box from "@mui/material/Box"
@@ -41,13 +41,43 @@ const products = [
         thumbnail: "https://t3.ftcdn.net/jpg/03/25/61/62/360_F_325616239_jPMacbnlr5hNYLBOazcKMtwvLWGKUEy3.jpg",
         id: 4,
     },
+    {
+        title: "Johny Earings",
+        price: 24,
+        inCart: false,
+        categories: ["Cute", "Earings", "Kawaii"],
+        colors: ["red", "blue", "green"],
+        thumbnail: "https://t3.ftcdn.net/jpg/03/25/61/62/360_F_325616239_jPMacbnlr5hNYLBOazcKMtwvLWGKUEy3.jpg",
+        id: 5,
+    },
+    {
+        title: "Johny Earings",
+        price: 24,
+        inCart: false,
+        categories: ["Cute", "Earings", "Kawaii"],
+        colors: ["red", "blue", "green"],
+        thumbnail: "https://t3.ftcdn.net/jpg/03/25/61/62/360_F_325616239_jPMacbnlr5hNYLBOazcKMtwvLWGKUEy3.jpg",
+        id: 6,
+    },
+    {
+        title: "Johny Earings",
+        price: 24,
+        inCart: false,
+        categories: ["Cute", "Earings", "Kawaii"],
+        colors: ["red", "blue", "green"],
+        thumbnail: "https://t3.ftcdn.net/jpg/03/25/61/62/360_F_325616239_jPMacbnlr5hNYLBOazcKMtwvLWGKUEy3.jpg",
+        id: 7,
+    },
 ]
 
 export default function ProductListView() {
     return (
         <Container maxWidth="lg" sx={{paddingY: 4}}>
-            <Stack flexWrap={true} flexDirection="row" sx={{gap:2}}>
+            <Stack flexWrap="wrap" flexDirection="row" sx={{ display: { xs: 'none', md: 'flex' }, gap:2, justifyContent: "space-around"}}>
                 {products.map((info) => <ProductItem key={info.id} info={info} />)}
+            </Stack>
+            <Stack id="batata" flexWrap="wrap" flexDirection="column" sx={{display: { xs: 'flex', md: 'none' }, gap:2, justifyContent: "space-around"}}>
+                {products.map((info) => <ProductItemStretch key={info.id} info={info} />)}
             </Stack>
         </Container>
     )
