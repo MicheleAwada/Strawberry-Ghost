af0b53ada505ca8e4ec022639c39bafd7964d6fd
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-07 09:48:52 +0200

    added header

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,29 +1,15 @@
 import { useContext } from "react";
-import { MessagesContext } from "../main"
+import { MessagesContext } from "../main";
 
-import Button from "@mui/material/Button"
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Box from "@mui/material/Box"
-import Typography from '@mui/material/Typography'
-
+import Header from "../components/header";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 export default function Index() {
-    const simpleAddMessage = useContext(MessagesContext);
-    return (
-        <Box>
-            {/* <AppBar position="fixed" color="primary">
-              <Toolbar>
-                <Typography variant="h6">
-                    Hello
-                </Typography>
-                <Button color="primary" onClick={() => simpleAddMessage("Hello")}>Say Hello</Button>
-              </Toolbar>
-            </AppBar> */}
-            <Button variant="contained" onClick={() => simpleAddMessage("HI")}>Say HI</Button>
-            <Button variant="contained" onClick={() => simpleAddMessage("HELLO")}>Say HELLO</Button>
-            <Button variant="contained" onClick={() => simpleAddMessage("BONJOUR")}>Say BONJOUR</Button>
-            
-        </Box>
-    )
+	const simpleAddMessage = useContext(MessagesContext);
+	return (
+		<Box>
+			<Header />
+		</Box>
+	);
 }
