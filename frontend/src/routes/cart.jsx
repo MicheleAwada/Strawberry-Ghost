a26a8cdee325c06502c64b6142a62607e9d9f824
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-09 13:47:15 +0200

    added cart page

--- frontend/src/routes/cart.jsx
+++ frontend/src/routes/cart.jsx
@@ -1,16 +1,51 @@
-import Box from '@mui/material/Box'
-import Container from '@mui/material/Container'
 import productListView from '../components/productListView'
 import ProductListView from '../components/productListView'
 import { getCartProducts } from '../fakeApi'
 import { useLoaderData } from "react-router-dom"
+
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+import useMediaQuery from "@mui/material/useMediaQuery"
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+
+import { Link as ReactRouterLink } from "react-router-dom"
 
 export async function loader() {
     return await getCartProducts()
 }
 export default function Cart() {
     const productsInCart = useLoaderData()
+    const isSm = useMediaQuery(theme => theme.breakpoints.up('sm'))
     return (
-          <ProductListView products={productsInCart} />
+          <Container maxWidth="lg">
+                <Typography variant={ isSm ? "h4" : "h5" } component="h5" color="initial" sx={{py: {xs: "2rem", sm: "2.5rem", md: "3rem"}, textAlign: "center"}}>
+                    You have {productsInCart.length} items in your cart
+                </Typography>
+              <Divider
+                variant="middle"
+                orientation="horizontal"
+                light={false}
+              />
+              <Stack gap={4} flexDirection="row" justifyContent="center" alignItems="center" sx={{ my: 2 }}>
+                  <Button variant="contained" size='large' color="primary"  startIcon={<ShoppingCartCheckoutIcon />}>
+                    Checkout
+                  </Button>
+                  <Button variant="outlined" size='large' color="primary"  startIcon={<AddShoppingCartIcon />} LinkComponent={ReactRouterLink} to="/" >
+                    More Shopping
+                  </Button>
+              </Stack>
+              <Divider
+                variant="middle"
+                orientation="horizontal"
+                light={false}
+              />
+              <ProductListView products={productsInCart} />
+          </Container>
     )
 }
