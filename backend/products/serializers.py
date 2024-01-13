ea1084d95c2adb3bde37aaf3b267aa9bb5d8f933
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-13 20:44:05 +0200

    functional many to many relation with product

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -8,11 +8,40 @@ class VariantImageSerializer(serializers.ModelSerializer):
         model = models.VariantImage
 
 class VariantSerializer(serializers.ModelSerializer):
+    images = VariantImageSerializer(many=True, read_only=False)
+
     class Meta:
         fields = ["id", "color", "name", "default", "images"]
         model = models.Variant
 
+    def validate_images(self, value):
+        if not (len(value) > 0):
+            raise serializers.ValidationError("Number of variants must be more than 0.")
+        return value
+
+    def create(self, validated_data):
+        images_data = validated_data.pop('images')
+        variant = models.Variant.objects.create(**validated_data)
+        image_serializer = VariantImageSerializer(data=images_data, many=True)
+        image_serializer.is_valid(raise_exception=True)
+        image_serializer.save(variant=variant)
+        return variant
+
 class ProductSerializer(serializers.ModelSerializer):
+    variants = VariantSerializer(many=True, read_only=False)
     class Meta:
-        fields = ["id", "title", "description", "price", "thumbnail"]
+        fields = ["id", "title", "description", "price", "thumbnail", "variants"]
         model = models.Product
+    def validate_variants(self, value):
+        if not (len(value) > 0):
+            raise serializers.ValidationError("Number of variants must be more than 0.")
+        return value
+
+    def create(self, validated_data):
+        variants_data = validated_data.pop('variants')
+        product = models.Product.objects.create(**validated_data)
+
+        variant_serializer = VariantSerializer(data=variants_data, many=True)
+        variant_serializer.is_valid(raise_exception=True)
+        variant_serializer.save(product=product)
+        return product
