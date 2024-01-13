fd02ecde035db3c0059c44281aff2d3d452b8e2f
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-13 20:56:22 +0200

    cleaner product view using useRenderImages

--- frontend/src/routes/productView.jsx
+++ frontend/src/routes/productView.jsx
@@ -17,6 +17,8 @@ import { flattenArray } from '../utils'
 import { useEffect, useRef, useState } from 'react'
 import {useLoaderData} from "react-router-dom"
 
+import useImageRendering from '../components/useImageRendering'
+
 import FrequentlyBoughtTogether from "../components/frequentlyBoughtTogether";
 
 
@@ -27,55 +29,16 @@ export async function loader({params}) {
     return product
 }
 
-function ImageRendering( querySelectorAll, imageSrcsIterable ) {
-    const [loadAllImages, setLoadAllImages] = useState(false)
-    let visibleImagesArray = useRef([])
-    function checkImagesLoaded() {
-        const allLoaded = visibleImagesArray.current.every((image) => image.complete);
-        setLoadAllImages(allLoaded);
-      };
-    function addImagesEventListeners() {
-        visibleImagesArray.current.forEach((image) => {
-            image.addEventListener('load', checkImagesLoaded);
-            image.addEventListener('error', checkImagesLoaded);
-          });
-    }
-    function removeImagesEventListeners() {
-        visibleImagesArray.current.forEach((image) => {
-            image.removeEventListener('load', checkImagesLoaded);
-            image.removeEventListener('error', checkImagesLoaded);
-          });
-    }
-    function setVisibleImages(querySelectorAll) {
-        const images = document.querySelectorAll(querySelectorAll);
-        const imagesArray = Array.from(images);
-        visibleImagesArray.current = imagesArray
-    }
-    
-    const RenderToLoadImages = () => {
-        return loadAllImages ? <div style={{ display: "none", flexDirection: "row", flexWrap: "wrap", gap: "1rem"}}>{imageSrcsIterable.map((imageSrc, index) => <img style={{width: "100px", height: "100px"}} key={imageSrc} className="currentColorImages" src={imageSrc} alt="hidden image for loading"/>)}</div> : null
-    }
-    const refreshClasses = (newQuerySelectorAll=querySelectorAll) => {
-          removeImagesEventListeners()
-          setVisibleImages(newQuerySelectorAll)
-          addImagesEventListeners()
-          return removeImagesEventListeners
-        }
-    useEffect(refreshClasses, []);
-    return [RenderToLoadImages, refreshClasses]
-}
-
-export default function ProductView() {
-    const product = useLoaderData();
-
+export function ProductDetail({ product }) {
     const [selectedImage, setSelectedImage] = useState(0)
     const [selectedVariant, setSelectedVariant] = useState(0)
 
     
-    const imageSrcsIterable = flattenArray(product.variants.map((variant) => variant.images.map((imageSrc) => imageSrc)))
-    const [RenderImageRendering, refreshClasses] = ImageRendering(".currentColorImages", imageSrcsIterable)
+    const imageSrcsIterable = flattenArray(product.variants.map((variant) => variant.images.map((image) => image.image)))
+    const [RenderImageRendering, refreshClasses] = useImageRendering(".currentColorImages", imageSrcsIterable)
     useEffect(refreshClasses, [selectedVariant])
 
+    const allImageAlt = `${product.title}'s image`
 
     function getSelectedColor() {
         return product.variants[selectedVariant]
@@ -84,15 +47,21 @@ export default function ProductView() {
         return getSelectedColor().images
     }
     function getSelectedImageSrc() {
-        return getImages()[selectedImage]
+        return getImages()[selectedImage].image
+    }
+    function getImageAlt(image) {
+        if (image.alt!==undefined) {
+            return image.alt
+        }
+        return allImageAlt
     }
     function RenderSelectImages({...props}) {
         return (<Stack flexDirection="row" justifyContent="center" flexWrap="wrap" gap={4} {...props}>
-            {getImages().map((imageSrc, index) => {
+            {getImages().map((image, index) => {
                 const borderColorSpread = {}
                 if (index === selectedImage) {borderColorSpread.borderColor = "primary.main"}
                 return <Paper component="button" onClick={() => setSelectedImage(index)} key={[selectedVariant, index]} variant='outlined' sx={{ borderRadius: "0.5rem", overflow: "hidden", aspectRatio: "1/1", width:"3rem", height: "3rem",  p: "1rem", boxSizing: "content-box", cursor: "pointer", ...borderColorSpread }}>
-                            <img loading='eager' className='currentColorImages' src={imageSrc} alt={imageAlt} style={{ aspectRatio: "1/1", objectFit: "cover", width: "100%", height: "100%", borderRadius: "0.25rem" }} />
+                            <img loading='eager' className='currentColorImages' src={image.image} alt={getImageAlt(image)} style={{ aspectRatio: "1/1", objectFit: "cover", width: "100%", height: "100%", borderRadius: "0.25rem" }} />
                         </Paper>
     })}
         </Stack>)
@@ -115,7 +84,7 @@ export default function ProductView() {
                             if (currentVariantImageLength <= selectedImage) {
                                 setSelectedImage(0)
                             }
-                        }} key={variant.id} variant="elevation" elevation={selectedVariant===index ? 6 : 1} sx={{borderRadius: "0.75rem", width: "auto", px:"1rem", py: "0.75rem", overflow: "hidden", cursor: "pointer", boxSizing: "content-box", borderStyle: "none" }}>
+                        }} key={variant.id || index} variant="elevation" elevation={selectedVariant===index ? 6 : 1} sx={{borderRadius: "0.75rem", width: "auto", px:"1rem", py: "0.75rem", overflow: "hidden", cursor: "pointer", boxSizing: "content-box", borderStyle: "none" }}>
                                     <Stack alignItems="center" justifyContent="center" flexDirection="row" sx={{width: "100%", height: "auto", }}>
                                         <Typography component="p" variant='body2'>{variant.name}</Typography>
                                         {variant.isColor && <><Divider flexItem orientation="vertical" variant="fullWidth" light sx={{mx: 1}} />
@@ -127,53 +96,62 @@ export default function ProductView() {
         </>
         )
     }
+    return <>
+        <Grid container spacing={8}  flexWrap={{xs:"wrap-reverse", md:"wrap"}}>
+            <Grid xs={12} md={6} lg={6} item>
+                <Stack gap={4}>
+                    <Box sx={{overflow: "hidden", aspectRatio: "4/3", borderRadius: "1rem", width: "100%", }}>
+                        <img src={getSelectedImageSrc()} alt={allImageAlt} style={{ width: "100%", objectFit: "cover"}} />
+                    </Box>
+                    <RenderSelectImages />
+                    <Box sx={{display: {xs: "block", md: "none"}}}>
+                        <ColorSelectDivider sx={{mb: "1.5rem"}} />
+                        <RenderVariantSelect />
+                    </Box>
+                </Stack>
+            </Grid>
+            <Grid xs={12} md={6} lg={6} item>
+                <Box>
+                    <Typography variant="h2" component={"h1"} gutterBottom color="initial">{product.title}</Typography>
+                    <Typography variant="body1" component={"p"} color="initial">{product.description}</Typography>
+                    <Box sx={{display: {xs: "none", md: "block"}}}>
+                        <ColorSelectDivider sx={{ my: {md: "1.5rem",lg: "2rem"} }} />
+                        <RenderVariantSelect />
+                    </Box>
+                    <Divider sx={{my: {xs:"1.5rem", md: "2rem", lg: "2.5rem"}}} />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} lg={6}>
+                            <Button variant="contained" color="primary" sx={{width: "100%"}} startIcon={<ShoppingCartCheckoutIcon />}>
+                                Buy Now
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} lg={6}>
+                            <Button variant="outlined" color="primary" sx={{width: "100%"}} startIcon={<AddShoppingCartIcon />}>
+                                Add to Cart
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Grid>
+        </Grid>
+        <RenderImageRendering />
+    </>
+}
 
-    const imageAlt = `${product.title}'s image`
+
+
+
+export default function ProductView({ product=null }) {
+    if (product===null) product = useLoaderData();
+
     return (
         <>
             <Container maxWidth="xl" sx={{py:"1rem"}}>
                 <Stack>
-                    <Grid container spacing={8}  flexWrap={{xs:"wrap-reverse", md:"wrap"}}>
-                        <Grid xs={12} md={6} lg={6} item>
-                            <Stack gap={4}>
-                                <Box sx={{overflow: "hidden", aspectRatio: "4/3", borderRadius: "1rem", width: "100%", }}>
-                                    <img src={getSelectedImageSrc()} alt={imageAlt} style={{ width: "100%", objectFit: "cover"}} />
-                                </Box>
-                                <RenderSelectImages />
-                                <Box sx={{display: {xs: "block", md: "none"}}}>
-                                    <ColorSelectDivider sx={{mb: "1.5rem"}} />
-                                    <RenderVariantSelect />
-                                </Box>
-                            </Stack>
-                        </Grid>
-                        <Grid xs={12} md={6} lg={6} item>
-                            <Box>
-                                <Typography variant="h2" component={"h1"} gutterBottom color="initial">{product.title}</Typography>
-                                <Typography variant="body1" component={"p"} color="initial">{product.description}</Typography>
-                                <Box sx={{display: {xs: "none", md: "block"}}}>
-                                    <ColorSelectDivider sx={{ my: {md: "1.5rem",lg: "2rem"} }} />
-                                    <RenderVariantSelect />
-                                </Box>
-                                <Divider sx={{my: {xs:"1.5rem", md: "2rem", lg: "2.5rem"}}} />
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12} lg={6}>
-                                        <Button variant="contained" color="primary" sx={{width: "100%"}} startIcon={<ShoppingCartCheckoutIcon />}>
-                                            Buy Now
-                                        </Button>
-                                    </Grid>
-                                    <Grid item xs={12} lg={6}>
-                                        <Button variant="outlined" color="primary" sx={{width: "100%"}} startIcon={<AddShoppingCartIcon />}>
-                                            Add to Cart
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Grid>
-                    </Grid>
+                    <ProductDetail product={product} />
                     <FrequentlyBoughtTogether sx={{ mt: "5rem" }} product={product} />
                 </Stack>
             </Container>
-            <RenderImageRendering />
         </>
     )
 }
