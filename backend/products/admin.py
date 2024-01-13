bd1f69baf4333c9426f220ab2eeb1926082a0c18
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-13 20:41:55 +0200

    registered models to admin

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Product)
+admin.site.register(models.CartItem)
+admin.site.register(models.OrderProductItem)
+admin.site.register(models.OrderItem)
+admin.site.register(models.VariantImage)
+admin.site.register(models.Variant)
