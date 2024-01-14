9591cb8f1ee57a13375f193aac8de6c342e7bba4
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-14 20:51:13 +0200

    added user router viewset to url

---
+++ backend/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from . import views
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'user', views.UserViewSet)
+
+
+urlpatterns = [
+
+]
+
+urlpatterns += router.urls
