5551814cbc688e25888c6c983d7dd48854c677f4
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-14 20:48:14 +0200

    myuserserializer and cart serializer

---
+++ backend/users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.apps import apps
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+CartItemModel = apps.get_model('products', 'CartItem')
+UserModel = get_user_model()
+from django.contrib.auth.models import Group
+class CartSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = CartItemModel
+        fields = ("id", "quantity", "user", "variant", "product")
+
+
+class MyUserSerializer(serializers.ModelSerializer):
+    cartitem_set = CartSerializer(many=True, read_only=True)
+    class Meta:
+        model = UserModel
+        fields = ("id", "email", "first_name", "last_name", "cartitem_set", "auth_token")
