eac53c0687b800266ceeb22a54e4df3335073073
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-14 12:28:33 +0200

    neater admin with handlers in seperate component page

---
+++ frontend/src/components/adminHandlers.jsx
@@ -0,0 +1,137 @@
+function baseProductChange(newChange, setProduct) {
+    setProduct((oldProduct => ({...oldProduct, ...newChange})))
+}
+function handleChangeTitle(event, setProduct) {
+    baseProductChange({title: event.target.value}, setProduct)
+}
+function handleChangeDescription(event, setProduct) {
+    baseProductChange({description: event.target.value}, setProduct)
+}
+
+function handleChangePrice(event, setProduct) {
+    let value = `${event.target.value}`
+    baseProductChange({price: value}, setProduct)
+}
+function handleChangeThumbnail(event, setProduct) {
+    const [file] = event.target.files
+    if (file) {
+        const url = URL.createObjectURL(file)
+        baseProductChange({thumbnail: url}, setProduct)
+    }
+}
+function handleAddVariant(event, setProduct) {
+    setProduct((product) => ({...product, variants: [
+        ...product.variants,
+        defaultVariant
+    ]}))
+}
+function baseChangeVariant(variantIndex, newVariant, setProduct) {
+        setProduct(oldProduct => {
+            return {
+                ...oldProduct,
+                variants: [
+                  ...oldProduct.variants.slice(0, variantIndex),
+                  {
+                    ...oldProduct.variants[variantIndex],
+                    ...newVariant
+                  },
+                  ...oldProduct.variants.slice(variantIndex + 1)
+                ]
+              }
+        }) 
+    }
+
+function handleVariantName(event, variantIndex, setProduct) {
+    baseChangeVariant(variantIndex, {name: event.target.value}, setProduct)
+}
+function handleVariantColor(event, variantIndex, setProduct) {
+    baseChangeVariant(variantIndex, {color: event.target.value}, setProduct)
+}
+function handleVariantIsColor(event, variantIndex, product, setProduct) {
+    baseChangeVariant(variantIndex, {isColor: !product.variants[variantIndex].isColor}, setProduct)
+}
+
+
+
+function variantImageBaseChange(newChange, variantIndex, imageIndex, setProduct) {
+    setProduct((oldProduct) => {
+        return {
+            ...oldProduct,
+            variants: [
+            ...oldProduct.variants.slice(0, variantIndex),
+            {
+                ...oldProduct.variants[variantIndex],
+                images: [
+                    ...oldProduct.variants[variantIndex].images.slice(0,imageIndex),
+                    {...oldProduct.variants[variantIndex].images[imageIndex], ...newChange},
+                    ...oldProduct.variants[variantIndex].images.slice(imageIndex+1),
+                  ]
+            },
+            ...oldProduct.variants.slice(variantIndex + 1)
+            ]
+      }
+    })
+}
+
+function handleVariantAddImage(event, variantIndex, imageIndex, setProduct) {
+    // image index shall be the length of the variant images list before we add the images
+    variantImageBaseChange(defaultVariant, variantIndex, imageIndex, setProduct)
+}
+
+
+function handleVariantChangeImage(event, variantIndex, imageIndex, setProduct) {
+    const [file] = event.target.files
+    if (file) {
+        const url = URL.createObjectURL(file)
+        variantImageBaseChange({image: url}, variantIndex, imageIndex, setProduct)
+    }
+}
+function handleVariantImageAlt(event, variantIndex, imageIndex, setProduct) {
+    variantImageBaseChange({alt: event.target.value}, variantIndex, imageIndex, setProduct)
+}
+
+
+
+const defaultImage = {
+    image: "https://creativelittlewomen.com/wp-content/uploads/2021/11/IMG_2439.jpg",
+    alt: "",
+}
+const defaultVariant = {
+        images: [
+            defaultImage,
+        ],
+        default: false,
+        isColor: false,
+        color: "",
+        name: ""
+}
+
+function defaultProduct(products) {
+    const highestId = products.reduce((max, product) => {
+        const id = product.id
+        if (id>max) {
+            return id
+        }
+        return max
+    }, 0)
+
+
+   return {
+        id: (highestId+1),
+        title: "",
+        description: "",
+        price: "",
+        frequentlyBoughtTogether: [],
+        variants: [
+            defaultVariant,
+        ],
+        thumbnail: "https://creativelittlewomen.com/wp-content/uploads/2021/11/IMG_2439.jpg"
+    }
+
+}
+
+
+export { defaultProduct,
+    handleChangeTitle, handleChangeDescription, handleChangePrice, handleChangeThumbnail,
+    handleAddVariant, handleVariantName, handleVariantColor, handleVariantIsColor,
+    handleVariantAddImage, handleVariantChangeImage, handleVariantImageAlt }
--- frontend/src/routes/admin.jsx
+++ frontend/src/routes/admin.jsx
@@ -13,7 +13,7 @@ import ProductView from "./productView"
 import ProductListView from "../components/productListView"
 
 import FileInput from "../components/fileInput"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useMemo, useState } from "react"
 import Divider from '@mui/material/Divider'
 import { getProducts } from "../fakeApi"
 
@@ -21,16 +21,31 @@ import Spinner from "../components/spinner"
 import { createProduct } from "../api"
 import { MessagesContext } from "../root"
 
+import { defaultProduct,
+    handleChangeTitle, handleChangeDescription, handleChangePrice, handleChangeThumbnail,
+    handleAddVariant, handleVariantName, handleVariantColor, handleVariantIsColor,
+    handleVariantAddImage, handleVariantChangeImage, handleVariantImageAlt } from "../components/adminHandlers"
+
+
 export async function loader() {
     const products = await getProducts()
     return products
 }
 export async function action({ request }) {
     const formData = await request.formData()
-    const isColor = formData.get("isColor")
-    formData.delete("isColor")
-    if (!isColor) formData.set("color", "")
-    console.log(formData)
+
+    // const isColor = formData.get("isColor")
+    // formData.delete("isColor")
+    // if (!isColor) formData.set("color", "")
+    console.log("hi")
+    try {
+        const stringPrice = formData.get("price")
+        const price = parseFloat(stringPrice)
+        formData.set("price", price)
+    } catch (e) {
+        return {succeeded: false, error: "Price Must Be a Decimal", response: null}
+    }
+
     const response = await createProduct(formData)
     return response
 }
@@ -54,222 +69,69 @@ export default function Admin() {
     }, [actionData])
 
 
-    const highestId = products.reduce((max, product) => {
-        const id = product.id
-        if (id>max) {
-            return id
-        }
-        return max
-    }, 0)
+    const [product, setProduct] = useState(defaultProduct(products))
 
-    const defaultImage = {
-        image: "https://creativelittlewomen.com/wp-content/uploads/2021/11/IMG_2439.jpg",
-        alt: "",
-    }
-    const defaultVariant = {
-            images: [
-                defaultImage,
-            ],
-            default: false,
-            isColor: false,
-            color: "",
-            name: ""
-    }
-    const [product, setProduct] = useState({
-        id: (highestId+1),
-        title: "",
-        description: "",
-        price: "",
-        frequentlyBoughtTogether: [],
-        variants: [
-            defaultVariant,
-        ],
-        thumbnail: "https://creativelittlewomen.com/wp-content/uploads/2021/11/IMG_2439.jpg"
-    })
-
-    function handleChangeBase(newChange) {
-        setProduct((oldProduct => ({...oldProduct, ...newChange})))
-    }
-    function handleChangeTitle(event) {
-        handleChangeBase({title: event.target.value})
-    }
-    function handleChangeDescription(event) {
-        handleChangeBase({description: event.target.value})
-    }
-
-    function handleChangePrice(event) {
-        let value = `${event.target.value}`
-        const oldValue = `${product.price}`
-        function getLastAddedLetter(str1, str2) {
-            if (str1.length === str2.length - 1 && str2.slice(0, -1) === str1) {
-              return str2.slice(-1);
-            } else {
-              return null;
-            }
-          }
-        if (getLastAddedLetter(oldValue, value)===".") {
-            value += "00"
-        }
-        handleChangeBase({price: value})
-    }
-    function handleChangeThumbnail(event) {
-        const [file] = event.target.files
-        if (file) {
-            const url = URL.createObjectURL(file)
-            handleChangeBase({thumbnail: url})
-        }
-    }
-    function handleAddVariant() {
-        setProduct((product) => ({...product, variants: [
-            ...product.variants,
-            defaultVariant
-        ]}))
-    }
-    function changeVariant(variantIndex, newVariant) {
-            setProduct(oldProduct => {
-                return {
-                    ...oldProduct,
-                    variants: [
-                      ...oldProduct.variants.slice(0, variantIndex),
-                      {
-                        ...oldProduct.variants[variantIndex],
-                        ...newVariant
-                      },
-                      ...oldProduct.variants.slice(variantIndex + 1)
-                    ]
-                  }
-            }) 
-        }
-    function handleVariantAddImage(event, variantIndex) {
-        setProduct((oldProduct) => {
-            return {
-                ...oldProduct,
-                variants: [
-                ...oldProduct.variants.slice(0, variantIndex),
+    
+    const variantsInForm = useMemo(() => {
+        return product.variants.map((variant, variantIndex) => (
+            <Stack gap="2rem" sx={{pl: {xs: "2rem", sm: "3rem", md: "4rem", lg: "6rem"}}} key={variantIndex}>
+                <TextField name={`variants[${variantIndex}][name]`} value={variant.name} label="Variant's Name" onChange={e => handleVariantName(e, variantIndex, setProduct)} required />
+                <FormControlLabel
+                    control={
+                        <Switch name="isColor" checked={variant.isColor} onChange={e => handleVariantIsColor(e, variantIndex, product, setProduct)} />
+                    }
+                    label="Is a color variant?"
+                />
+                {/* "isColor" name for isColor but we remove it since its not a actual backend field */}
                 {
-                    ...oldProduct.variants[variantIndex],
-                    images: [
-                        ...oldProduct.variants[variantIndex].images,
-                        defaultImage
-                      ]
-                },
-                ...oldProduct.variants.slice(variantIndex + 1)
-                ]
-          }
-        })
-    }
-    function handleVariantChangeImage(event, variantIndex, imageIndex) {
-        const [file] = event.target.files
-        let url = product.variants[variantIndex].images[imageIndex].image
-        if (file) {
-            url = URL.createObjectURL(file)
-        }
-        setProduct((oldProduct) => {
-            return {
-                ...oldProduct,
-                variants: [
-                ...oldProduct.variants.slice(0, variantIndex),
-                {
-                    ...oldProduct.variants[variantIndex],
-                    images: [
-                        ...oldProduct.variants[variantIndex].images.slice(0,imageIndex),
-                        {...oldProduct.variants[variantIndex].images[imageIndex], image: url},
-                        ...oldProduct.variants[variantIndex].images.slice(imageIndex+1),
-                      ]
-                },
-                ...oldProduct.variants.slice(variantIndex + 1)
-                ]
-          }
-        })
-    }
-    function handleVariantImageAlt(event, variantIndex, imageIndex) {
-        setProduct((oldProduct) => {
-            return {
-                ...oldProduct,
-                variants: [
-                ...oldProduct.variants.slice(0, variantIndex),
-                {
-                    ...oldProduct.variants[variantIndex],
-                    images: [
-                        ...oldProduct.variants[variantIndex].images.slice(0,imageIndex),
-                        {...oldProduct.variants[variantIndex].images[imageIndex], alt: event.target.value},
-                        ...oldProduct.variants[variantIndex].images.slice(imageIndex+1),
-                      ]
-                },
-                ...oldProduct.variants.slice(variantIndex + 1)
-                ]
-          }
-        })
-    }
+                    variant.isColor &&
+                        <TextField name={`variants[${variantIndex}][color]`} label="choose variant color" type="color" onChange={e => handleVariantColor(e, variantIndex, setProduct)} value={variant.color} />
+                }
+                <Divider
+                    variant="fullWidth"
+                    orientation="horizontal"
+                    
+                ><Typography>This Variants Images</Typography></Divider>
+                {variant.images.map((image, imageIndex) => (
+                    <Stack gap="2rem" key={imageIndex} sx={{ pl: {xs: "2rem", sm: "3rem", md: "4rem", lg: "6rem"} }} flexWrap="wrap" flexDirection="row">
+                        <TextField name={`variants[${variantIndex}][images][${imageIndex}][alt]`} onChange={e => handleVariantImageAlt(e, variantIndex, imageIndex, setProduct)} value={image.alt} label="Name for Image" id="form-admin-product-variants-images-image-alt" />
+                        <FileInput text="Upload Image for Variant *" name={`variants[${variantIndex}][images][${imageIndex}][image]`} inputProps={{onChange: e => handleVariantChangeImage(e, variantIndex, imageIndex, setProduct), required: true, accept: "image/*" }} id={`variant image ${variantIndex} ${imageIndex}`} />
+                    </Stack>
+                ))}
+                <Stack alignItems="center">
+                    <Button variant="contained" onClick={e => {handleVariantAddImage(e, variantIndex, variant.images.length, setProduct)}}>Add Image For Variant</Button>
+                </Stack>
+            </Stack>
+        ))
+    }, [product.variants])
+    const form = <ReactRouterForm encType="multipart/form-data" method="POST">
+    <Stack gap="2rem" mb="4rem">
+        <TextField name="title" id="form-admin-product-title" value={product.title} onChange={e => handleChangeTitle(e, setProduct)} label="Title" variant="outlined" required />
+        <TextField name="description" id="form-admin-product-description" multiline minRows={4} maxRows={14} value={product.description} onChange={e => handleChangeDescription(e, setProduct)} label="Description" variant="outlined" required />
+        <TextField name="price" id="form-admin-product-price" value={product.price} onChange={e => handleChangePrice(e, setProduct)} label="Price" variant="outlined" required />
+        <Stack justifyContent="center" alignItems="center">
+            <FileInput name="thumbnail" text="Upload Main Thumbnail *" id="form-admin-product-thumbnail" inputProps={{onChange: e => handleChangeThumbnail(e, setProduct), required: true, accept: "image/*"}} />
+        </Stack>
+        <Divider
+          variant="fullWidth"
+          orientation="horizontal"
+          
+        ><Typography>Variants</Typography></Divider>
+        {variantsInForm}
 
-    function handleVariantName(event, variantIndex) {
-        changeVariant(variantIndex, {name: event.target.value})
-    }
-    function handleVariantColor(event, variantIndex) {
-        changeVariant(variantIndex, {color: event.target.value})
-    }
-    function handleVariantIsColor(event, variantIndex) {
-        changeVariant(variantIndex, {isColor: !product.variants[variantIndex].isColor})
-    }
-
-
+        <Stack alignItems="center">
+            <Button variant="contained" onClick={e => handleAddVariant(e, setProduct)}>Add Variant</Button>
+        </Stack>
+    </Stack>
+    {/* <Box sx={{bgcolor: "#000000"}}>
+        <Spinner />
+    </Box> */}
+    <Button type="sumbit" fullWidth variant="contained" startIcon={isLoading ? <Spinner /> : undefined}>SUBMIT</Button>
+</ReactRouterForm>
 
     return (
         <Container maxWidth="lg" sx={{ pt: "4rem" }}>
-            <ReactRouterForm encType="multipart/form-data" method="POST">
-                <Stack gap="2rem" mb="4rem">
-                    <TextField name="title" id="form-admin-product-title" value={product.title} onChange={handleChangeTitle} label="Title" variant="outlined" required />
-                    <TextField name="description" id="form-admin-product-description" multiline minRows={4} maxRows={14} value={product.description} onChange={handleChangeDescription} label="Description" variant="outlined" required />
-                    <TextField name="price" id="form-admin-product-price" value={product.price} onChange={handleChangePrice} label="Price" variant="outlined" required />
-                    <Stack justifyContent="center" alignItems="center">
-                        <FileInput name="thumbnail" text="Upload Main Thumbnail *" id="form-admin-product-thumbnail" inputProps={{onChange: handleChangeThumbnail, required: true, accept: "image/*"}} />
-                    </Stack>
-                    <Divider
-                      variant="fullWidth"
-                      orientation="horizontal"
-                      
-                    ><Typography>Variants</Typography></Divider>
-                    {product.variants.map((variant, variantIndex) => (
-                        <Stack gap="2rem" sx={{pl: "6rem"}} key={variantIndex}>
-                            <TextField name={`variants[${variantIndex}][name]`} value={variant.name} label="Variant's Name" onChange={(e) => handleVariantName(e, variantIndex)} required />
-                            <FormControlLabel
-                                control={
-                                    <Switch name="isColor" checked={variant.isColor} onChange={(e) => handleVariantIsColor(e, variantIndex)} />
-                                }
-                                label="Is a color variant?"
-                            />
-                            {/* "isColor" name for isColor above since its not a actual backend field */}
-                            {
-                                variant.isColor ?
-                                    <TextField name={`variants[${variantIndex}][color]`} label="choose variant color" type="color" onChange={e => handleVariantColor(e, variantIndex)} value={variant.color} />
-                                    : <TextField name={`variants[${variantIndex}][color]`} type="hidden" sx={{display: "none"}} value={"null"}  />
-                            }
-                            <Divider
-                                variant="fullWidth"
-                                orientation="horizontal"
-                                
-                            ><Typography>This Variants Images</Typography></Divider>
-                            {variant.images.map((image, imageIndex) => (
-                                <Stack gap="2rem" key={imageIndex} sx={{ pl: "6rem" }} flexWrap="wrap" flexDirection="row">
-                                    <TextField name={`variants[${variantIndex}][images][${imageIndex}][alt]`} onChange={e => handleVariantImageAlt(e, variantIndex, imageIndex)} value={image.alt} label="Name for Image" id="form-admin-product-variants-images-image-alt" />
-                                    <FileInput text="Upload Image for Variant *" name={`variants[${variantIndex}][images][${imageIndex}][image]`} inputProps={{onChange: e => handleVariantChangeImage(e, variantIndex, imageIndex), required: true, accept: "image/*" }} id={`variant image ${variantIndex} ${imageIndex}`} />
-                                </Stack>
-                            ))}
-                            <Stack alignItems="center">
-                                <Button variant="contained" onClick={e => {handleVariantAddImage(e, variantIndex)}}>Add Image</Button>
-                            </Stack>
-                        </Stack>
-                    ))}
-
-                    <Stack alignItems="center">
-                        <Button variant="contained" onClick={handleAddVariant}>Add Variant</Button>
-                    </Stack>
-                </Stack>
-                {/* <Box sx={{bgcolor: "#000000"}}>
-                    <Spinner />
-                </Box> */}
-                <Button type="sumbit" fullWidth variant="contained" startIcon={isLoading ? <Spinner /> : undefined}>SUBMIT</Button>
-            </ReactRouterForm>
+            {form}
             <Divider
                       variant="inset"
                       orientation="horizontal"
@@ -280,7 +142,7 @@ export default function Admin() {
                       variant="inset"
                       orientation="horizontal"
                       sx={{ my: "4rem" }}
-                    >How this Product will look like In the HomePage with The other products (current product is the first)</Divider>
+                    ><Typography>How this Product will look like In the HomePage with The other products (current product is the first)</Typography></Divider>
             <ProductListView products={[product, ...products]} />
         </Container>
     )
