3f78829aa9bfc49f6ce9f31a54e67fd8ad56945c
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-14 20:49:48 +0200

    added google login view

--- backend/users/views.py
+++ backend/users/views.py
@@ -61,3 +61,34 @@ class login(ObtainAuthToken):
         # userdata.is_valid(raise_exception=True)
         return Response(userdata.data)
 
+
+# google_client_id = settings.GOOGLE_CLIENT_ID
+# google_secret = settings.GOOGLE_CLIENT_SECRET
+google_client_id = "batata"
+google_secret = "batata"
+class GoogleAuth(APIView):
+    def post(self, request):
+        data = request.data
+        token = data.get('credential')
+        try:
+            idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), google_client_id)
+
+            if not (idinfo["aud"] == google_client_id == data.get("clientId")):
+                raise ValidationError("Invalid client ID.")
+        except:
+            # Invalid token
+            return Response({"status": "invalid token"}, status=400)
+        else:
+            userid = str(idinfo['sub'])
+            email = idinfo.get('email')
+            name = idinfo.get("name")
+            google_users_group, created = Group.objects.get_or_create(name="google_users")
+
+
+            user = google_users_group.user_set.filter(google_id=userid)
+            user_exists = user.exists()
+            if user_exists:# login account
+                user = user.first()
+            else: # create account
+                user = UserModel.objects.create_user(email=email, name=name, password="",)
+            return return_user_data(user)
