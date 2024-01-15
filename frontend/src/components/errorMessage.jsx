91cdcaf90f0d1453795ca50b5eafb00cbca11a9d
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-15 14:54:18 +0200

    added isError attr for get error

--- frontend/src/components/errorMessage.jsx
+++ frontend/src/components/errorMessage.jsx
@@ -1,19 +1,26 @@
-function getErrorMessage(error, itemLocation) {
+function getFullError(error, itemLocation) {
+    const returnError = {error: "", isError: false}
     const keys = itemLocation.split(/\]\[|\[|\]/).filter(Boolean);
     let isError = false;
     let currentError = error;
     for (const key of keys) {
       if (currentError[key] === undefined) {
-        return ""; // Item doesnt exist
+        return returnError; // Item doesnt exist
       }
       currentError = currentError[key];
     }
     if (!Array.isArray(currentError)) {
-        return ""
+        return returnError
     }
     isError = true;
     
     let stringError = currentError.join(", ");
     return {error: stringError, isError: isError};
   }
-export {getErrorMessage}
+function getErrorMessage(error, itemLocation) {
+    return getFullError(error, itemLocation).error;
+}
+function getIsError(error, itemLocation) {
+    return getFullError(error, itemLocation).isError;
+}
+export {getFullError, getErrorMessage, getIsError}
