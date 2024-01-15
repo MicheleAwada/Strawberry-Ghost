b37e4464800b15a363247bf5dc32e2b32fffb857
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-15 14:58:44 +0200

    added error to fileinput

--- frontend/src/components/fileInput.jsx
+++ frontend/src/components/fileInput.jsx
@@ -1,20 +1,27 @@
 import Button from "@mui/material/Button"
+import Stack from "@mui/material/Stack"
+import Typography from '@mui/material/Typography'
 
 
-export default function FileInput({ text="Upload Image", name, id, inputProps, buttonProps }) {
+export default function FileInput({ text="Upload Image", error, name, id, inputProps, buttonProps }) {
     return (
         <label htmlFor={id}>
                     <input
-                        style={{ display: 'none' }}
+                        style={{ opacity: '0', width:"1px", height:"1px" }}
                         id={id}
                         name={name}
                         type="file"
                         {...inputProps}
                     />
 
-                    <Button color="primary" variant="contained" component="span" {...buttonProps}>
-                        {text}
-                    </Button>
+                    <Stack>
+                        <Button color="primary" variant="contained" component="span" {...buttonProps}>
+                            {text}
+                        </Button>
+                        {error && <Typography variant="body1" color="initial">
+                            {error}
+                        </Typography>}
+                    </Stack>
                 </label>
     )
 }
