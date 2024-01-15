b17c81737642752ec74e14cf534d97e674de7742
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-15 21:03:12 +0200

    added login to urls

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,12 +2,13 @@ from django.urls import path,include
 from . import views
 from rest_framework import routers
 
+
 router = routers.SimpleRouter()
 router.register(r'user', views.UserViewSet)
 
 
 urlpatterns = [
-
+    path("login/", views.ObtainAuthToken.as_view()),
 ]
 
 urlpatterns += router.urls
