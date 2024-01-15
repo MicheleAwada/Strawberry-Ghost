53bd5e756c7a95a9365c6f3d422c1795e6bc4a9b
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-15 19:16:55 +0200

    added cart to urls

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, CartViewSet
 
 router = SimpleRouter()
 router.register(r"product", ProductViewSet)
+router.register(r"cart", CartViewSet)
 
 
 urlpatterns = [
