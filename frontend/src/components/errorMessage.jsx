135e7ab14eadc1ab4f0572084b19450d6994dbfe
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-15 14:56:30 +0200

    get error bug fixes

--- frontend/src/components/errorMessage.jsx
+++ frontend/src/components/errorMessage.jsx
@@ -13,18 +13,17 @@ function formatError(errorArray) {
   }
 
 function getFullError(error, itemLocation) {
-    const returnError = {error: "", isError: false}
     const keys = itemLocation.split(/\]\[|\[|\]/).filter(Boolean);
     let isError = false;
     let currentError = error;
     for (const key of keys) {
       if (currentError[key] === undefined) {
-        return returnError; // Item doesnt exist
+        return {error: null, isError: isError}; // Item doesnt exist
       }
       currentError = currentError[key];
     }
     if (!Array.isArray(currentError)) {
-        return returnError
+        return {error: currentError, isError: isError};
     }
     isError = true;
     
@@ -32,9 +31,9 @@ function getFullError(error, itemLocation) {
     return {error: stringError, isError: isError};
   }
 function getErrorMessage(error, itemLocation) {
-    return getFullError(error, itemLocation)[0];
+    return getFullError(error, itemLocation).error;
 }
 function getIsError(error, itemLocation) {
-    return getFullError(error, itemLocation)[0];
+    return getFullError(error, itemLocation).isError;
 }
 export {getFullError, getErrorMessage, getIsError, formatError}
