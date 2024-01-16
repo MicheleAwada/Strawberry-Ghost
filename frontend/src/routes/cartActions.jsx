a1398f7350ef66c7fdc828a1032eff1ac622e156
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-16 17:26:33 +0200

    fixed cart action bug

--- frontend/src/routes/cartActions.jsx
+++ frontend/src/routes/cartActions.jsx
@@ -1,17 +1,17 @@
 import { addCartItem } from "../api"
 
 export async function addToCartAction({ request }) {
-    const formData = await request.formData
+    const formData = await request.formData()
     const response = addCartItem(formData)
     return response
 }
 export async function putCartAction({ request }) {
-    const formData = await request.formData
+    const formData = await request.formData()
     const response = addCartItem(formData)
     return response
 }
 export async function deleteCartAction({ request }) {
-    const formData = await request.formData
+    const formData = await request.formData()
     const response = addCartItem(formData)
     return response
 }
