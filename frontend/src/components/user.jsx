be83e59e56818439c170f5ce25d50c7028037fe2
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-16 17:26:23 +0200

    better user context with base user

--- frontend/src/components/user.jsx
+++ frontend/src/components/user.jsx
@@ -1,11 +1,13 @@
 import { createContext, useState } from "react";
 import { getUser } from "../fakeApi";
 
-
-export const UserContext = createContext([getUser(), () => {}]);
+const baseUser = {
+    is_authenticated: false
+}
+export const UserContext = createContext([baseUser, () => {}]);
 
 export default function UserProvider({ children }) {
-    const [user, setUser] = useState(getUser())
+    const [user, setUser] = useState(baseUser)
     return (
         <UserContext.Provider value={[user, setUser]}>
             {children}
