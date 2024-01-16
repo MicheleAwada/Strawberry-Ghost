e3c11e560552557805329c9271bd7d31ae353c18
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-16 16:00:49 +0200

    added email verification model to models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
+from django.core.exceptions import ValidationError
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -40,3 +43,43 @@ class UserPayment(models.Model):
     card_number = models.CharField(null=True, blank=True, max_length=20)
     card_holder_name = models.CharField(null=True, blank=True, max_length=100)
     card_cvv = models.CharField(null=True, blank=True, max_length=10)
+
+class EmailVerification(models.Model):
+    email = models.EmailField(blank=False)
+    token = models.CharField(max_length=100)
+    attempts = models.IntegerField(default=0)
+    blocked = models.BooleanField(default=False)
+
+    time_created = models.DateTimeField(auto_now_add=True)
+    def is_expired(self):
+        current_time = timezone.now()
+        x_minutes_ago = current_time - timezone.timedelta(minutes=5)
+
+        return self.time_created < x_minutes_ago
+    def too_much_attempts(self, raise_exception=True):
+        if self.attempts > 30 or self.blocked:
+            if raise_exception: raise ValidationError("Cannot verify, too much attempts, please contact us")
+            self.attempts +=1
+            return True
+        return False
+    def is_valid(self, token, raise_exception=True):
+        if self.token != token:
+            self.attempts += 1
+            if raise_exception: raise ValidationError("Invalid verification code")
+            return False
+        self.too_much_attempts()
+        if self.is_expired():
+            self.attempts += 1
+            if raise_exception: raise ValidationError("Cannot verify, token expired")
+            return False
+        return True
+    def is_valid_to_mail(self, raise_exception=True):
+        self.attempts += 1
+        if self.too_much_attempts(raise_exception):
+            return False
+        return True
+    def generate_token(self):
+        import random
+        random_6_digit = random.randint(100000, 999999)
+        self.token = str(random_6_digit)
+        return self.token
