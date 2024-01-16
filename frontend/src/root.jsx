d59cd74415f2a20aaf91c4883ec49d1a97f92896
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-16 16:11:57 +0200

    setuser if authed in root

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -2,24 +2,34 @@ import {Outlet, useLoaderData} from "react-router"
 import { RenderMessages, MessagesContext } from "./components/messages"
 
 import { UserContext } from "./components/user"
-import { getProducts, getUser } from "./fakeApi"
+import { getProducts } from "./fakeApi"
 
 import Header from "./components/header"
 
 import Box from "@mui/material/Box";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Footer from "./components/footer"
 
 import ScrollToTop from "./components/scrollToTop"
 
+import { getUser, is_authenticated, set_token } from "./api"
 
 
 export async function loader() {
     getProducts() // to load products from the beggining if the user opened a non products page
-    return null
+    let user = {};
+    if (is_authenticated()) {
+        user = await getUser()
+    }
+    return user
 }
 
 export default function Root() {
+    const gotUser = useLoaderData()
+    const [user, setUser] = useContext(UserContext)
+    useEffect(() => {if (gotUser.response) {
+        setUser({...gotUser.response, is_authenticated: true})
+    }}, [])
     ScrollToTop()
     return (
         <Box>
