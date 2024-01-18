0e18b1319e8a1e26fa75506d6272d111a2907b69
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 21:25:55 +0200

    google logout on logout

--- frontend/src/routes/logout.jsx
+++ frontend/src/routes/logout.jsx
@@ -6,6 +6,9 @@ import { baseUser } from "../components/user"
 import { MessagesContext } from "../root"
 import {useNavigate} from "react-router-dom"
 
+import { googleLogout } from '@react-oauth/google';
+
+
 export default function Logout() {
     const [user, setUser] = useContext(UserContext)
     const {simpleAddMessage} = useContext(MessagesContext)
@@ -13,6 +16,9 @@ export default function Logout() {
     function onLogout() {
         const wasAuthenticated = user.is_authenticated
         logout()
+        if (user.is_google_user) {
+            googleLogout();
+        }
         setUser(baseUser)
         wasAuthenticated ? simpleAddMessage("Successfully Logged Out") : simpleAddMessage("You're already logged out!", {severity: "info"})
         navigate("/")
