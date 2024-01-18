cf66f254c092b1da8a68377a98f0ae21ceb1371d
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 09:54:50 +0200

    logout page

---
+++ frontend/src/routes/logout.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react"
+import { logout } from "../api"
+import Button from '@mui/material/Button'
+import { UserContext } from "../components/user"
+import { baseUser } from "../components/user"
+import { MessagesContext } from "../root"
+import {useNavigate} from "react-router-dom"
+
+export default function Logout() {
+    const [user, setUser] = useContext(UserContext)
+    const {simpleAddMessage} = useContext(MessagesContext)
+    const navigate = useNavigate();
+    function onLogout() {
+        const wasAuthenticated = user.is_authenticated
+        logout()
+        setUser(baseUser)
+        wasAuthenticated ? simpleAddMessage("Successfully Logged Out") : simpleAddMessage("You're already logged out!", {severity: "info"})
+        navigate("/")
+
+    }
+    return (
+        <Button variant="contained" color="primary" onClick={onLogout}>
+            logout
+        </Button>
+    )
+}
