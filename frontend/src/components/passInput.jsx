50fe070502520d40c33e3a3d3d3aebfaeffcadba
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 21:24:02 +0200

    password input component added

---
+++ frontend/src/components/passInput.jsx
@@ -0,0 +1,36 @@
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useState } from 'react';
+
+export default function PassInput({children, ...props}) {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <TextField
+            id="passowrd"
+            label="Password"
+            type={showPassword ? "text" : "password"}
+            InputProps={{
+            endAdornment: (
+                <InputAdornment position="end">
+                <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+                </InputAdornment>
+            ),
+            }}
+            {...props}
+            >{children}</TextField>
+    )
+}
