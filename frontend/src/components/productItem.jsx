a44d7e7ac943fc949ef49d24e80056858b0f68cd
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 10:05:05 +0200

    api add to cart in product item

--- frontend/src/components/productItem.jsx
+++ frontend/src/components/productItem.jsx
@@ -9,6 +9,7 @@ import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 
 
 import useMediaQuery from "@mui//material/useMediaQuery"
@@ -16,7 +17,7 @@ import useMediaQuery from "@mui//material/useMediaQuery"
 import { Link as ReactRouterLink, useFetcher } from "react-router-dom"
 import IconButton from '@mui/material/IconButton'
 import Spinner from "./spinner";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "./user";
 import { MessagesContext } from "./messages";
 
@@ -63,32 +64,45 @@ export function ProductPrice({ price, sx, textColor="initial", ...props }) {
 }
 
 const DefaultProductCardActions = ({product}) => {
-	const [user] = useContext(UserContext)
+	const [user, setUser] = useContext(UserContext)
 	const is_authenticated = user.is_authenticated
 
 	const { simpleAddMessage } = useContext(MessagesContext)
 
 	const defaultQuantity = 1
-	const defaultVariant = product.variants.find((variant) => variant.default) || product.variants[0]
+	const initalVariantsId = product.variants[0].id
+	function getCartItem() {
+		if (!user.is_authenticated) return false
+		const cartItem = user.cartitem_set.find((cartItem) => initalVariantsId === cartItem.variant) || false
+		return cartItem
+	}
+	const [cartItem, setCartItem] = useState(getCartItem())
+
 
 	const fetcher = useFetcher();
 	const sumbitting = fetcher.state === "submitting";
+	useEffect(() => {
+		if (fetcher.data && fetcher.data.succeeded) {
+			cartItem ? simpleAddMessage("Removed from cart", {severity: "success"}) : simpleAddMessage("Woohoo, added to cart", {severity: "success"})
+			setUser(oldUser => ({...oldUser, ...fetcher.data.response}))
+		}
+	}, [fetcher.data])
 
 	useEffect(() => {
-		console.info(fetcher.data)
-		console.info(fetcher.formData)
-	}, [fetcher])
+		setCartItem(getCartItem())
+	}, [user])
 
-	return <fetcher.Form action="/addToCart" method="POST">
-		<input type="hidden" name="quantity" value={1} />
-		<input type="hidden" name="variant" value={product.variants[0].id} />
+	return <fetcher.Form action={cartItem ? "/deleteCart" : "/addToCart"} method={cartItem ? "DELETE" : "POST"}>
+		<input type="hidden" name="id" value={cartItem ? cartItem.id : 0} />
+		<input type="hidden" name="quantity" value={defaultQuantity} />
+		<input type="hidden" name="variant" value={initalVariantsId} />
 		<input type="hidden" name="product" value={product.id} />
 		<IconButton color="primary" aria-label="add to cart" type={is_authenticated ? "submit" : "button"} onClick={() => {
 			if (!is_authenticated) {
 				simpleAddMessage("You cannot add to cart before you login", {severity: "error"})
 			}
 		}} >
-			{sumbitting ? <Spinner /> : <AddShoppingCartIcon />}
+			{sumbitting ? <Spinner /> : cartItem ? <RemoveShoppingCartIcon /> : <AddShoppingCartIcon />}
 			</IconButton>
 	</fetcher.Form>
 }
@@ -96,7 +110,7 @@ const DefaultProductCardExtras = ({ children }) => <Stack flexDirection="row" fl
 
 export default function ProductItem({ product, titleVariant=null, ProductCartActions=DefaultProductCardActions, ProductCardExtras = DefaultProductCardExtras }) {
     const isMd = useMediaQuery(theme => theme.breakpoints.up('md'));
-	const productLink = `/products/${product.id}`
+	const productLink = `/products/${product.slug}`
 
 	if (!titleVariant) titleVariant = isMd ? "h5" : "h6"
 
