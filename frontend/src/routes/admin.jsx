966d443108363e4a93ae42a29ac557611e88ba07
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 09:55:32 +0200

    up to front in admin page feature added

--- frontend/src/routes/admin.jsx
+++ frontend/src/routes/admin.jsx
@@ -10,6 +10,7 @@ import IconButton from '@mui/material/IconButton'
 
 import PlusIcon from "@mui/icons-material/AddCircle"
 import RemoveIcon from "@mui/icons-material/RemoveCircle"
+import UpIcon from '@mui/icons-material/ArrowCircleUp';
 
 import { Form as ReactRouterForm, useActionData, useLoaderData, useNavigation } from "react-router-dom"
 
@@ -17,7 +18,7 @@ import ProductView from "./productView"
 import ProductListView from "../components/productListView"
 
 import FileInput from "../components/fileInput"
-import { useContext, useEffect, useMemo, useState } from "react"
+import { useContext, useEffect, useMemo, useRef, useState } from "react"
 import Divider from '@mui/material/Divider'
 import { getProducts } from "../fakeApi"
 
@@ -28,8 +29,8 @@ import { MessagesContext } from "../root"
 import { defaultProduct,
     handleVerifySlug,
     handleChangeTitle, handleChangeDescription, handleChangePrice, handleChangeSlug, handleChangeThumbnail,
-    handleVariantAdd, handleVariantRemove, handleVariantName, handleVariantColor, handleVariantIsColor,
-    handleVariantImageAdd, handleVariantImageRemove, handleVariantChangeImage, handleVariantImageAlt } from "../components/adminHandlers"
+    handleVariantAdd, handleVariantRemove, handleVariantToFront, handleVariantName, handleVariantColor, handleVariantIsColor,
+    handleVariantImageAdd, handleVariantImageRemove, handleVariantImageToFront, handleVariantChangeImage, handleVariantImageAlt, } from "../components/adminHandlers"
 
 import { getFullError } from "../components/errorMessage"
 
@@ -47,13 +48,11 @@ export async function action({ request }) {
 
     const slug = formData.get("slug")
     if (!handleVerifySlug(slug)) {
-        console.log("oh no")
         return {succeeded: false, error: {slug: ["Slug Must Be only Alphanumeric and -"]},errorMessage: "Slug Must Be only Alphanumeric and -", response: null}
     }
 
 
 
-    console.log(formData)
 
     const response = await createProduct(formData)
     return response
@@ -81,14 +80,43 @@ export default function Admin() {
 
 
     const [product, setProduct] = useState(defaultProduct(products))
-    const [demo, setDemo] = useState(false)
+
+
+    const [lookingAtDemo, setLookingAtDemo] = useState(false)
+    const [productDemo, setProductDemo] = useState({...product})
+    const productDemoRef = useRef();
     useEffect(() => {
-        if (demo===true) setDemo(false)
-    }, [product])
+        isElementInView(productDemoRef.current)}
+    , [])
+    useEffect(() => {
+        if (lookingAtDemo===true) {
+            setProductDemo({...product})
+        }
+    }, [product, lookingAtDemo])
+
+    function isElementInView(element) {
+        const options = {
+          root: null,
+          rootMargin: '0px',
+          threshold: 0,
+        };
+      
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                setLookingAtDemo(true)
+            } else {
+                setLookingAtDemo(false)
+            }
+          });
+        }, options);
+      
+        observer.observe(element);
+      }
+
 
     function addFromName(name) {
         const fullError = getFullError(error, name)
-        console.log(fullError)
         return {name: name, helperText: fullError.error, error: fullError.isError}
     }
     const inputVariant = "filled"
@@ -96,12 +124,12 @@ export default function Admin() {
         <Container maxWidth="lg" sx={{ pt: "4rem" }}>
             <ReactRouterForm encType="multipart/form-data" method="POST">
                 <Stack gap="2rem" mb="6rem">
-                    <TextField {...addFromName("title")} helperText id="form-admin-product-title" value={product.title} onChange={e => handleChangeTitle(e, setProduct)} label="Title" variant={inputVariant} required />
+                    <TextField {...addFromName("title")} id="form-admin-product-title" value={product.title} onChange={e => handleChangeTitle(e, setProduct)} label="Title" variant={inputVariant} required />
                     <TextField {...addFromName("description")} id="form-admin-product-description" multiline minRows={4} maxRows={14} value={product.description} onChange={e => handleChangeDescription(e, setProduct)} label="Description" variant={inputVariant} required />
                     <TextField {...addFromName("price")} id="form-admin-product-price" value={product.price} onChange={e => handleChangePrice(e, product, setProduct)} label="Price" variant={inputVariant} required />
                     <TextField {...addFromName("slug")} id="form-admin-product-slug" value={product.slug} onChange={e => handleChangeSlug(e, setProduct)} label="Slug" variant={inputVariant} required />
                     <Stack alignItems="start">
-                        <FileInput name="thumbnail" text="Upload Main Thumbnail *" id="form-admin-product-thumbnail" inputProps={{onChange: e => handleChangeThumbnail(e, setProduct), required: true, accept: "image/*"}} />
+                        <FileInput name="thumbnail" text="Upload Main Thumbnail *" id="form-admin-product-thumbnail" inputProps={{onChange: e => handleChangeThumbnail(e, setProduct), required: true, accept: "image/*"}} url={product.thumbnail} />
                     </Stack>
                     <Divider
                     variant="fullWidth"
@@ -114,9 +142,13 @@ export default function Admin() {
                                 <IconButton  aria-label="add variant" onClick={e => handleVariantAdd(e, variantIndex, setProduct)}>
                                     <PlusIcon />
                                 </IconButton>
-                                {product.variants.length!==1 && <IconButton aria-label="delete" onClick={e => handleVariantRemove(e, variantIndex, setProduct)}>
+                                {product.variants.length!==1 && <><IconButton aria-label="delete" onClick={e => handleVariantRemove(e, variantIndex, setProduct)}>
                                     <RemoveIcon />
-                                </IconButton>}
+                                </IconButton>
+                                <IconButton aria-label="move to front" onClick={e => handleVariantToFront(e, variantIndex, setProduct)}>
+                                    <UpIcon />
+                                </IconButton>
+                                </>}
                             </Stack>
                             <TextField {...addFromName(`variants[${variantIndex}][name]`)} value={variant.name} label="Variant's Name" onChange={e => handleVariantName(e, variantIndex, setProduct)} variant={inputVariant} required />
                             <Stack>
@@ -134,14 +166,18 @@ export default function Admin() {
                             {variant.images.map((image, imageIndex) => (
                                 <Stack gap="2rem" key={imageIndex} sx={{ pl: {xs: "2rem", sm: "3rem", md: "4rem", lg: "6rem"} }} flexWrap="wrap" flexDirection="row" alignItems="center">
                                     <TextField {...addFromName(`variants[${variantIndex}][images][${imageIndex}][alt]`)} onChange={e => handleVariantImageAlt(e, variantIndex, imageIndex, setProduct)} value={image.alt} label="Name for Image" id="form-admin-product-variants-images-image-alt" variant={inputVariant} />
-                                    <FileInput text="Upload Image for Variant *" name={`variants[${variantIndex}][images][${imageIndex}][image]`} inputProps={{onChange: e => handleVariantChangeImage(e, variantIndex, imageIndex, setProduct), required: true, accept: "image/*" }} id={`variant image ${variantIndex} ${imageIndex}`} />
-                                    <Stack flexDirection="row">
+                                    <FileInput text="Upload Image for Variant *" name={`variants[${variantIndex}][images][${imageIndex}][image]`} inputProps={{onChange: e => handleVariantChangeImage(e, variantIndex, imageIndex, setProduct), required: true, accept: "image/*" }} url={image.image} id={`variant image ${variantIndex} ${imageIndex}`} />
+                                    <Stack flexDirection="row" alignItems="center">
                                         <IconButton sx={{aspectRatio: "1/1"}} aria-label="add image to variant" onClick={e => handleVariantImageAdd(e, variantIndex, imageIndex, setProduct)}>
                                           <PlusIcon />
                                         </IconButton>
-                                        {variant.images.length!==1 && <IconButton aria-label="delete image to variant" onClick={e => handleVariantImageRemove(e, variantIndex, imageIndex, setProduct)}>
+                                        {variant.images.length!==1 && <><IconButton aria-label="delete image to variant" onClick={e => handleVariantImageRemove(e, variantIndex, imageIndex, setProduct)}>
                                           <RemoveIcon />
-                                        </IconButton>}
+                                        </IconButton>
+                                        <IconButton aria-label="move this variant's image to front" onClick={e => handleVariantImageToFront(e, variantIndex, imageIndex, setProduct)}>
+                                          <UpIcon />
+                                        </IconButton>
+                                        </>}
                                     </Stack>
                                 </Stack>
                             ))}
@@ -152,18 +188,18 @@ export default function Admin() {
                 <Button type="sumbit" fullWidth variant="contained" startIcon={isLoading ? <Spinner /> : undefined}>SUBMIT</Button>
             </ReactRouterForm>
             <Button type="button" sx={{ mt: "2rem" }} onClick={e => setDemo(true)} fullWidth variant="contained">VIEW DEMO!!</Button>
-            {demo && <><Divider
+            <Box ref={productDemoRef}><Divider
                       variant="inset"
                       orientation="horizontal"
                       sx={{ my: "4rem" }}
                     ><Typography>How this Product will look like</Typography></Divider>
-            <ProductView product={product} />
+            <ProductView product={productDemo} />
             <Divider
                       variant="inset"
                       orientation="horizontal"
                       sx={{ my: "4rem" }}
                     ><Typography>How this Product will look like In the HomePage with The other products (current product is the first)</Typography></Divider>
-            <ProductListView products={[product, ...products]} /></>}
+            <ProductListView products={[productDemo, ...products]} /></Box>
         </Container>
     )
 }
