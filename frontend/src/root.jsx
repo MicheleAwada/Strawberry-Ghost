08232d7140d6c904f90f9a6769e4ef304cb19531
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-18 10:03:36 +0200

    real api get user in root

--- frontend/src/root.jsx
+++ frontend/src/root.jsx
@@ -2,7 +2,6 @@ import {Outlet, useLoaderData} from "react-router"
 import { RenderMessages, MessagesContext } from "./components/messages"
 
 import { UserContext } from "./components/user"
-import { getProducts } from "./fakeApi"
 
 import Header from "./components/header"
 
@@ -12,14 +11,17 @@ import Footer from "./components/footer"
 
 import ScrollToTop from "./components/scrollToTop"
 
-import { getUser, is_authenticated, set_token } from "./api"
+import { getUser, is_authenticated, logout, getProducts } from "./api"
 
 
 export async function loader() {
-    getProducts() // to load products from the beggining if the user opened a non products page
+    getProducts() // to load products from the beggining if the user opened a non products page but no asyncly
     let user = {};
     if (is_authenticated()) {
         user = await getUser()
+        if (!user.succeeded && user.errorType=="external") {
+            logout()
+        }
     }
     return user
 }
@@ -27,9 +29,11 @@ export async function loader() {
 export default function Root() {
     const gotUser = useLoaderData()
     const [user, setUser] = useContext(UserContext)
-    useEffect(() => {if (gotUser.response) {
-        setUser({...gotUser.response, is_authenticated: true})
-    }}, [])
+    useEffect(() => {
+        if (gotUser.response) {
+            setUser({...gotUser.response, is_authenticated: true})
+        }
+    }, [])
     ScrollToTop()
     return (
         <Box>
