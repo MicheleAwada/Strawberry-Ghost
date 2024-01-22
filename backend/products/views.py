1cb3a91b36dab1d88c9853985df4856711431a9d
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-01-22 12:48:42 +0200

    added nested formdata to update of product

--- backend/products/views.py
+++ backend/products/views.py
@@ -57,3 +57,19 @@ class ProductViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    def update(self, request, *args, **kwargs):
+        form = NestedForm(request.data)
+        form.is_nested(raise_exception=True)
+
+        partial = kwargs.pop('partial', False)
+
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=form.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
