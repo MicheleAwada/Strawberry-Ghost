f6946e65e258460c9b2a3f3ffe90733712332e77
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-02-20 14:47:20 +0200

    migrations and user tests

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+from django.core.files.uploadedfile import SimpleUploadedFile
+from products.models import Variant
+class MyModelTestCase(TestCase):
+    def test_default_avatar_added(self):
+        user = UserModel.objects.create(email='test@example.com')
+        print(user.has_bought_variant())
