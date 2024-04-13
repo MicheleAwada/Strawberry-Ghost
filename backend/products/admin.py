be3ceb8b2faf1eaa286fd76a5c0bcd4dbfb622b3
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-04-13 18:15:57 +0200

    customized order item model for admin

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -4,7 +4,16 @@ from . import models
 
 admin.site.register(models.Product)
 admin.site.register(models.CartItem)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("time_created", "status", "paid")
+    list_filter = ("time_created", "status", "paid")
+    search_fields = ("status",)
+
+admin.site.register(models.OrderItem, OrderItemAdmin)
 admin.site.register(models.OrderProductItem)
-admin.site.register(models.OrderItem)
+
+
+
 admin.site.register(models.VariantImage)
 admin.site.register(models.Variant)
