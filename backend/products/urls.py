6de5ccf64fbf20cf348ec806d6bc602a0247e2fa
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-04-21 19:19:05 +0200

    added orders to url

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 
 from rest_framework.routers import SimpleRouter
-from .views import ProductViewSet, CartViewSet
+from .views import ProductViewSet, CartViewSet, OrderViewSet
 
 router = SimpleRouter()
 router.register(r"product", ProductViewSet)
 router.register(r"cart", CartViewSet)
 
 
+
 urlpatterns = [
+    path("orders/", OrderViewSet.as_view()),
 ]
 
 urlpatterns += router.urls
