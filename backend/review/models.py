d235788f7221c68efa53591ab49aa2ef5faa3d13
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-14 08:24:26 +0200

    added full review model

--- backend/review/models.py
+++ backend/review/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+class Review(models.Model):
+    body = models.CharField(max_length=3000)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reviews")
+    variant = models.ForeignKey("products.Variant", on_delete=models.CASCADE, related_name="reviews")
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    purchase_date = models.DateField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ("user", "variant")
