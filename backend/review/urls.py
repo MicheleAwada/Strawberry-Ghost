53b50332608390ae5cf39f4df8d2a577a984e451
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-22 15:56:15 +0200

    added urls from viewset

--- backend/review/urls.py
+++ backend/review/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+from .views import ReviewViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"reviews", ReviewViewSet)
+
+urlpatterns = []
+urlpatterns += router.urls
