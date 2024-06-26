720006d5d329f765c41413283ef29e0bdb30a63b
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-26 08:59:55 +0200

    created contact model

--- backend/contact/models.py
+++ backend/contact/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Contact(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=100)
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, blank=True, null=True)
