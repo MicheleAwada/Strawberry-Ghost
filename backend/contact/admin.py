fcefd48da9bb42587d8a7e80cae0180e667f6198
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-06-29 12:09:47 +0200

    added customized contact model to admin

--- backend/contact/admin.py
+++ backend/contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("title", "email", "name")
+    list_filter = ("title", "email", "name")
+    list_display_links = ("title", "email", "name")
+
+admin.site.register(Contact, ContactAdmin)
