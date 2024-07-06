6fe84ae1eb3f69fcf1aae074a8235a691acbefca
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-07-06 18:18:34 +0200

    better settings.py with hidden secret key and using env,

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,23 +13,24 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-#PROD remove
-from dotenv import load_dotenv
-load_dotenv()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# from dotenv import load_dotenv
+# load_dotenv(BASE_DIR / "config" / ".env")
+# load_dotenv( BASE_DIR / "config" / ".db.env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbc*jvo&e(_pjl*e%ddas-7txv1-l96ed)z%87vgk65$@d*(vv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
