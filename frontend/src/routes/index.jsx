0e42b8c34c3af51e8b0ab40cccb62fbe0ae8624a
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-09-12 16:42:43 +0200

    better homepage

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,25 +1,44 @@
-import Button from '@mui/material/Button'
 
 import { MessagesContext } from '../root'
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { getProducts } from "../api";
 import { useLoaderData } from "react-router-dom"
 import ProductListView from '../components/productListView';
-import IndexCarousel from '../components/carousel';
+
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+import { HeaderHeightContext } from '../components/header';
 
 export async function loader() {
-    return await getProducts()
+    const b =  await getProducts()
+    console.log(b)
+    return b
 }
 
 
 export default function Index() {
     const products = useLoaderData();
-    const simpleAddMessage = useContext(MessagesContext).simpleAddMessage;
+    const { simpleAddMessage } = useContext(MessagesContext);
+    const [headerHeight] = useContext(HeaderHeightContext)
+    const productListViewRef = useRef()
+    function buttonOnClick() {
+        if (!productListViewRef.current) {
+            return;
+        }
+
+        const topPosition = productListViewRef.current.offsetTop - headerHeight;
+
+        window.scrollTo({
+            top: topPosition,
+            behavior: "smooth",
+        });
+    }
 	return (
         <>
-        <IndexCarousel />
-        <ProductListView products={products} />
+        <Box ref={productListViewRef}>
+            <ProductListView paginated={true} products={products} />
+        </Box>
         </>
 	)
 }
