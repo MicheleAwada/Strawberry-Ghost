4d9eca4026230d9142bfb8ce5220cb2d1b07f539
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-09-19 14:37:33 +0200

    better logout page

--- frontend/src/routes/logout.jsx
+++ frontend/src/routes/logout.jsx
@@ -1,6 +1,11 @@
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import Paper from '@mui/material/Paper'
+
 import { useContext } from "react"
 import { logout } from "../api"
-import Button from '@mui/material/Button'
 import { UserContext } from "../components/user"
 import { baseUser } from "../components/user"
 import { MessagesContext } from "../root"
@@ -25,8 +30,18 @@ export default function Logout() {
 
     }
     return (
-        <Button variant="contained" color="primary" onClick={onLogout}>
-            logout
-        </Button>
+        <Stack alignItems="center" justifyContent="center" sx={{width: "100%", height: "100%", py: {xs: "1rem", md: "2rem"}, px: {xs: "0rem", md: "2rem"}, boxSizing: "border-box"}}>
+            <Paper sx={{p: {xs: "1rem", sm: "2rem", md: "3rem", lg: "4rem"}}}>
+                    <Stack gap={4}>
+                        <Typography variant='h4' color="primary" sx={{ textAlign: "center", width: "25rem" }}>Are You Sure You Want To Logout?!</Typography>
+                        <Divider />
+                        <Stack alignItems="center">
+                            <Button variant="contained" color="primary" onClick={onLogout}>
+                                logout
+                            </Button>
+                        </Stack>
+                    </Stack>
+            </Paper>
+        </Stack>
     )
 }
