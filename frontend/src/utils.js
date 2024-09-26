06ccbbc6f9dd0208d3ba2a396fe366639540c6e1
MicheleAwada <75516947+MicheleAwada@users.noreply.github.com>
2024-09-26 15:38:57 +0200

    added more utils functions to utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -9,6 +9,33 @@ function flattenArray(array) {
     }, [])
 }
 
+function equalSplitArray(array, chunkSize) {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+}
 
+function getVariant(products, variantId) {
+    let variant;
+    products.some((product) => {
+        return product.variants.some((products_variant) => {
+            if (products_variant.id == variantId) {
+                variant = products_variant
+                return true
+            }
+            return false 
+        })
+    })
+    if (variant === undefined) {
+        throw new Error("Sorry something went wrong, the variant wasn't found, try refreshing and going back to home")
+    }
+    return variant
+}
 
-export {flattenArray}
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export { flattenArray, getVariant, sleep, equalSplitArray }
